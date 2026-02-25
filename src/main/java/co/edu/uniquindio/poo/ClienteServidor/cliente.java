38846b475dd2b3ef84a89280daa24e16da7d83fd
KarolMD <karolm.davilas@uqvirtual.edu.co>
2026-02-25 14:30:55 -0500

    Update cliente.java

--- src/main/java/co/edu/uniquindio/poo/ClienteServidor/cliente.java
+++ src/main/java/co/edu/uniquindio/poo/ClienteServidor/cliente.java
@@ -9,7 +9,7 @@ public class cliente {
     public static void main(String[] args) {
 
         // CAMBIA ESTA IP SI ES OTRO COMPUTADOR
-        String host = "192.168.1.15"; //ip servidor wifi ipconfig
+        String host = "192.168.201.83"; //ip servidor wifi ipconfig
         int puerto = 5000;
 
         Scanner scanner = new Scanner(System.in);
